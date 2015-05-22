516cbdbd062e52cc4d07191791a22d5994b6cef8
Gabriela Schirmer Mauricio <gabismauricio@gmail.com>
2015-05-22 10:46:42 -0300

    Skins e Particulas

--- SceneTest4/MySceneView.swift
+++ SceneTest4/MySceneView.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 import SceneKit
+import SpriteKit
 
 class MySceneView: SCNScene {
     
-//    let rotate: NSArray!
+//    var tete = "teste"
+    var sphereBackground : SCNBox!
+    //    let rotate: NSArray!
 
     func createSun(){
         let sun = SCNSphere(radius: 3.0)
-        sun.firstMaterial!.diffuse.contents = UIImage(named: "texture_sun.jpg")
+        sun.firstMaterial!.diffuse.contents = UIImage(named: "texture_sun.png")
         let sunNode = SCNNode(geometry: sun)
         let rotate = SCNAction.rotateByX(0, y: 1.0, z: 0, duration: 1.0)
         let repeatedRotate = SCNAction.repeatActionForever(rotate)
@@ -23,7 +26,7 @@ class MySceneView: SCNScene {
         self.rootNode.addChildNode(sunNode)
         
         let planet = SCNSphere(radius: 1.0)
-        planet.firstMaterial!.diffuse.contents = UIColor.grayColor()
+        planet.firstMaterial!.diffuse.contents = UIImage(named: "planet_water_texture.png")
         let planetNode = SCNNode(geometry: planet)
         let planetRotate = SCNAction.rotateByX(0, y: 1.0, z: 0, duration: 1.0)
         planetNode.position = SCNVector3Make(8, 0, 0)
@@ -31,41 +34,78 @@ class MySceneView: SCNScene {
         planetNode.runAction(repeatedPlanetRotate)
         sunNode.addChildNode(planetNode)
         
-        let titleText = SCNText(string: "My Title", extrusionDepth: 0.3)
-        let titleNode = SCNNode(geometry: titleText)
-        titleNode.position = SCNVector3Make(0, 1.0, 0)
-        titleNode.scale = SCNVector3Make(0.1, 0.1, 0.1)
-        let titleRotation = SCNAction.rotateByX(0, y: -1.0, z: 0, duration: 1.0)
-        let repeatedTextRotate = SCNAction.repeatActionForever(titleRotation)
-        titleNode.runAction(repeatedTextRotate)
-        planetNode.addChildNode(titleNode)
+        let cloud = SCNSphere(radius: 1.2)
+        cloud.firstMaterial!.diffuse.contents = UIImage(named: "cloud_texture.png")
+        let cloudNode = SCNNode(geometry: cloud)
+        let cloudRotate = SCNAction.rotateByX(0, y: 0.5, z: 0, duration: 1.0)
+        cloudNode.position = SCNVector3Make(8, 0, 0)
+        let repeatedCloudRotate = SCNAction.repeatActionForever(cloudRotate)
+        cloudNode.runAction(repeatedCloudRotate)
+        sunNode.addChildNode(cloudNode)
         
-        let moon = SCNSphere(radius: 0.3)
-        moon.firstMaterial!.diffuse.contents = UIColor.greenColor()
-        let moonNode = SCNNode(geometry: moon)
-        let moonRotate = SCNAction.rotateByX(0, y: 1.0, z: 0, duration: 1.0)
-        moonNode.position = SCNVector3Make(2, 0, 0)
-        let repeatedMoonRotate = SCNAction.repeatActionForever(moonRotate)
-        moonNode.runAction(repeatedMoonRotate)
-        planetNode.addChildNode(moonNode)
+//        var labelNode = SKLabelNode(text: "Hello")
+
+//        let titleText = SCNText(string: "My Title", extrusionDepth: 0.3)
+//        let titleNode = SCNNode(geometry: titleText)
+//        titleNode.position = SCNVector3Make(0, 1.0, 0)
+//        titleNode.scale = SCNVector3Make(0.1, 0.1, 0.1)
+//        let titleRotation = SCNAction.rotateByX(0, y: -1.0, z: 0, duration: 1.0)
+//        let repeatedTextRotate = SCNAction.repeatActionForever(titleRotation)
+//        titleNode.runAction(repeatedTextRotate)
+//        planetNode.addChildNode(titleNode)
+//        
+//        let moon = SCNSphere(radius: 0.3)
+//        moon.firstMaterial!.diffuse.contents = UIColor.greenColor()
+//        let moonNode = SCNNode(geometry: moon)
+//        let moonRotate = SCNAction.rotateByX(0, y: 1.0, z: 0, duration: 1.0)
+//        moonNode.position = SCNVector3Make(2, 0, 0)
+//        let repeatedMoonRotate = SCNAction.repeatActionForever(moonRotate)
+//        moonNode.runAction(repeatedMoonRotate)
+//        planetNode.addChildNode(moonNode)
         
-        let ring = SCNTube(innerRadius: 1.5, outerRadius: 2, height: 0.01)
-        ring.firstMaterial!.diffuse.contents = UIColor.brownColor()
+//        let ring = SCNTube(innerRadius: 3.5, outerRadius: 5, height: 0.01)
+        let ring = SCNPlane(width: 10, height: 10)
+        
+        ring.firstMaterial!.diffuse.contents = UIImage(named: "texture_ring.png")
+//        ring.firstMaterial?.diffuse.contents = UIColor.grayColor()
         let ringNode = SCNNode(geometry: ring)
+        ring.firstMaterial?.doubleSided = true
+
         let ringNode2 = SCNNode(geometry: ring)
-
-       // ringNode.position = SCNVector3Make(0, 0, 0)
+        
         var pifloat = Float(M_PI)
-        ringNode.transform = SCNMatrix4MakeRotation( pifloat / 3, 0, 1, 1)
-        ringNode2.transform = SCNMatrix4MakeRotation( 3*pifloat / 2, 0, 1, 1)
-        planetNode.addChildNode(ringNode)
-        planetNode.addChildNode(ringNode2)
+        ringNode.transform = SCNMatrix4MakeRotation( pifloat, 1, 1, 1)
+        ringNode2.transform = SCNMatrix4MakeRotation( 3*pifloat / 2, 1, 1, 1)
+        sunNode.addChildNode(ringNode)
+        sunNode.addChildNode(ringNode2)
+        
+    }
+    
+    func createbackground(){
+        
+        let particleSystem = SCNParticleSystem(named: "MyParticleSystem", inDirectory: nil)
+        let systemNode = SCNNode()
+        systemNode.addParticleSystem(particleSystem)
+        rootNode.addChildNode(systemNode)
 
+//        sphereBackground = SCNSphere(radius: 100)
+//        sphereBackground.firstMaterial?.diffuse.contents = UIImage(named: "universo1.tga")
+//        sphereBackground.firstMaterial?.transparency = 1.0
+//        sphereBackground.firstMaterial?.doubleSided = true
+//        let spherenode = SCNNode(geometry: sphereBackground)
+//        self.rootNode.addChildNode(spherenode)
+        
+//        sphereBackground = SCNBox(width: 100, height: 100, length: 100, chamferRadius: 10)
+////        sphereBackground.materials = [UIImage(named: "universe.png"), UIImage(named: "universe.png"), UIImage(named: "universe.png"), UIImage(named: "universe.png"), UIImage(named: "universe.png"), UIImage(named: "universe.png")]
+//        sphereBackground.firstMaterial?.doubleSided = true
+//        let spherenode = SCNNode(geometry: sphereBackground)
+//        self.rootNode.addChildNode(spherenode)
     }
     
     override init() {
         super.init()
         createSun()
+        createbackground()
     }
     
     required init(coder aDecoder: NSCoder) {
--- SceneTest4/ViewController.swift
+++ SceneTest4/ViewController.swift
@@ -20,56 +20,59 @@ class ViewController: UIViewController {
         
         scnView = self.view as! SCNView
         
-        scnView.scene = MySceneView()
-        
+        scnView.scene = MySceneView();
         scnView.backgroundColor = UIColor.blackColor()
+
+//        let cameraNode = SCNNode()
+//        cameraNode.camera = SCNCamera()
+//        cameraNode.position = SCNVector3(x: 0, y: 30, z: 30)
+//        cameraNode.rotation = SCNVector4Make(0, 1, 0, Float(M_PI/6))
+//        scnView.pointOfView = cameraNode
+        
+//        var image = UIImage(named: "universo.png")
+//        var foto = image?.CGImage
+
         scnView.autoenablesDefaultLighting = true
         scnView.allowsCameraControl = true
         
+//        let tap = UITapGestureRecognizer(target:self, action: "sceneTap:")
+//        tap.numberOfTapsRequired = 1
+//        tap.numberOfTouchesRequired = 1
+//        scnView.gestureRecognizers = [tap]
         
-        let tap = UITapGestureRecognizer(target:self, action: "sceneTap:")
-        tap.numberOfTapsRequired = 1
-        tap.numberOfTouchesRequired = 1
-        scnView.gestureRecognizers = [tap]
-        
-        
-        
-        let buttonGeometry = SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0)
-        let buttonMaterial = SCNMaterial()
-        buttonMaterial.diffuse.contents = UIColor.redColor()
-        buttonGeometry.materials = [buttonMaterial]
-        button = SCNNode(geometry: buttonGeometry)
-        button.position = SCNVector3(x: 5, y: 0.5, z: 15)
-        scnView.scene?.rootNode.addChildNode(button)
-
-    
+//        let buttonGeometry = SCNBox(width: 10, height: 10, length: 10, chamferRadius: 0.3)
+//        buttonGeometry.firstMaterial?.diffuse.contents = UIImage(named: "universo.png")
+//        button = SCNNode(geometry: buttonGeometry)
+//        button.position = SCNVector3(x: 5, y: 0.5, z: -30)
+//        scnView.scene?.rootNode.addChildNode(button)
+//        
     }
     
-    func sceneTap(recognizer: UITapGestureRecognizer) {
-        
-        let location = recognizer.locationInView(scnView)
-        
-        let hitResults = scnView.hitTest(location, options: nil)
-        
-        
-        if hitResults?.count > 0 {
-            
-            let result = hitResults![0] as! SCNHitTestResult
-            let node = result.node
-            if node == button {
-                //                SCNTransaction.begin()
-                //                SCNTransaction.setAnimationDuration(0.5)
-                //                let materials = node.geometry?.materials as! [SCNMaterial]
-                //                let material = materials[0]
-                //                material.diffuse.contents = UIColor.whiteColor()
-                //                SCNTransaction.commit()
-                //
-                //                let action = SCNAction.moveByX(0, y: -0.8, z: 0, duration: 0.5)
-                //                node.runAction(action)
-                println("oi")
-            }
-        }
-    }
+//    func sceneTap(recognizer: UITapGestureRecognizer) {
+//
+//        let location = recognizer.locationInView(scnView)
+//
+//        let hitResults = scnView.hitTest(location, options: nil)
+//
+//
+//        if hitResults?.count > 0 {
+//            
+//            let result = hitResults![0] as! SCNHitTestResult
+//            let node = result.node
+//            if node == button {
+//                //                SCNTransaction.begin()
+//                //                SCNTransaction.setAnimationDuration(0.5)
+//                //                let materials = node.geometry?.materials as! [SCNMaterial]
+//                //                let material = materials[0]
+//                //                material.diffuse.contents = UIColor.whiteColor()
+//                //                SCNTransaction.commit()
+//                //
+//                //                let action = SCNAction.moveByX(0, y: -0.8, z: 0, duration: 0.5)
+//                //                node.runAction(action)
+//                println("oi")
+//            }
+//        }
+//    }
 
 
     override func didReceiveMemoryWarning() {
